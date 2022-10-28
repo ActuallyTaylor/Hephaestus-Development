05805fdf8d59c796da8402056359216c89b7ae6d
Zachary Lineman <zachary.lineman@gmail.com>
2022-10-28 00:39:02 -0400

    Hopefully removed all cmake and build files

--- Archive/Engine/API/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/zacharylineman/GitHub/Hephaestus/Engine/API
-# Build directory: /Users/zacharylineman/GitHub/Hephaestus/Engine/API/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
